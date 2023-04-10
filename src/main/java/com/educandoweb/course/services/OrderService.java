c7590296a030c7b7b9d166ace02c94c5ccdeab78
Lucas Lapa <lucasrlapa@icloud.com>
2023-04-10 20:46:07 -0300

    Category

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.Order;
-import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.repositories.OrderRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
-	private UserRepository repository;
+	private OrderRepository repository;
 	
 	public List<Order> findAll() {
 		return repository.findAll();
